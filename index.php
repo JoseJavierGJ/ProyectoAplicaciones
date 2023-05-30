ac1bc1ac2d4919a2094ba0fe29da5adc44c17e57
YoselinEscudero <y.escuderohernandez@ugto.mx>
2023-05-30 14:06:52 -0600

    agregue texto

--- index.php
+++ index.php
@@ -4,11 +4,28 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>🐵🙊🙉</title>
+  <title>T-School</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-  <h1>Hola Mundo!!!!</h1>
-  <h1>Pull</h1>
-  
+  <nav>
+    <label class="logo">T-School</label>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">contac</a></li>
+      <li><a href="">Admission</a></li>
+      <li><a href="">Login</a></li>
+    </ul>
+  </nav>
+
+
 </body>
 </html>
