ea36e43432e71da53ce3dcfe872af9c6d1bf10ef
YoselinEscudero <y.escuderohernandez@ugto.mx>
2023-06-01 14:37:13 -0600

    agregue video 1 y 2

--- index.php
+++ index.php
@@ -29,6 +29,101 @@
       <label class="img_text">We Teach Students with care</label>
       <img class="main_img" src="1.jpg">
     </div>
+   <div class="container">
+      <div class="row">
+        <div class="col-md-4">
 
+          <img class ="welcome_img" src="school2.jpg" >
+
+        </div>
+
+        <div class="col-md-8">
+          <h1>Welcome to T-School</h1>
+          <p> Proporcionar soporte y orientación: Una página web educativa puede ofrecer soporte y 
+             orientación a los usuarios en su proceso de aprendizaje.
+             Esto puede incluir la disponibilidad de tutoriales, guías,
+             preguntas frecuentes, recursos adicionales y servicios de apoyo, como servicios de tutoría en línea o asesoramiento académico.</p>
+
+        </div>
+
+
+      </div>
+
+   </div>
+    <center>
+      <h1>Our Teachers</h1>
+    </center>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+            <img class="teacher" src="teacher1.jpg">
+            <p>in a vibrant, academically challenging, and encouraging
+              environment where manifols viewpoints are prized and celebrated
+            </p>
+        </div>
+        <div class="col-md-4">
+             <img class="teacher" src="teacher2.jpg">
+             <p>in a vibrant, academically challenging, and encouraging
+              environment where manifols viewpoints are prized and celebrated</p>
+        </div>
+        <div class="col-md-4">
+             <img class="teacher" src="teacher3.jpg">
+             <p>in a vibrant, academically challenging, and encouraging
+              environment where manifols viewpoints are prized and celebrated</p>
+
+        </div>
+      </div>
+    </div>
+    <center>
+      <h1>Our Courses</h1>
+    </center>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+            <img class="teacher" src="web.jpg">
+            <h3>Applications web</h3>
+        </div>
+        <div class="col-md-4">
+             <img class="teacher" src="graphic.jpg">
+             <h3>Graphics for computer</h3>
+             
+        </div>
+        <div class="col-md-4">
+             <img class="teacher" src="marketing.png">
+             <h3>Marketing</h3>
+             
+        </div>
+      </div>
+    </div>
+    <center>
+      <h1 class ="adm">Admission Form</h1>
+    </center>
+    <div align="center" class="admission_form">
+      <form>
+        <div class="adm_int">
+          <label class="label-text">Name</label>
+          <input class="input_Deg" type="text" name="">
+        </div>
+
+        <div class="adm_int">
+          <label class="label-text">Email</label>
+          <input class="input_Deg" type="text" name="">
+        </div>
+        <div class="adm_int">
+          <label class="label-text">Phone</label>
+          <input class="input_Deg" type="text" name="">
+        </div>
+        <div class="adm_int">
+          <label class="label-text">Message</label>
+          <textarea class="input_txt"></textarea>
+        </div>
+        <div class="adm_int">
+          <input class="btn btn-primary" type="submit" value="apply" id="submit">
+        </div>
+        <footer>
+          <h3 class="footer_text">All @copyright reserved by web tech knowledge </h3>
+        </footer>
+      </form>
+    </div>
 </body>
 </html>
