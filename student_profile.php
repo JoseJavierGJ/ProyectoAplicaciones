e335766f227192ce73f28e8fa0a72e85aeec136b
JoseJavierGJ <jjosejavier@hotmail.com>
2023-06-04 14:45:34 -0600

    Actualizar Perfil de Estudiante

--- student_profile.php
+++ student_profile.php
@@ -8,6 +8,31 @@ session_start();
   elseif($_SESSION['usertype']=='admin'){
     header("location:login.php");
   }
+
+  $host="localhost";
+  $user="root";
+  $password="";
+  $db="schoolproject";
+
+  $data=mysqli_connect($host, $user, $password, $db);
+  $name=$_SESSION['usertype'];
+  $sql="SELECT * FROM user WHERE username='$name' ";
+  $result=mysqli_query($data, $sql);
+  $info=mysqli_fetch_assoc($result);
+
+  if(isset($_POST['update_profile'])){
+    $s_email=$_POST['email'];
+    $s_phone=$_POST['phone'];
+    $s_password=$_POST['password'];
+
+    $sql2="UPDATE user SET email='$s_email', phone='$s_phone', password='$s_password' WHERE username='$name' ";
+    $result2=mysqli_query($data, $sql2);
+
+    if($result2){
+      header('location:student_profile.php');
+    }
+
+  }
 ?>
 
 
@@ -23,6 +48,23 @@ session_start();
     include 'student_css.php'
   ?>  
 
+  <style type="text/css" >
+    label{
+      display: inline-block;
+      text-align: right;
+      width: 100px;
+      padding-top: 10px;
+      padding-bottom: 10px;
+    }
+
+    .div_deg{
+      background-color: #f36bec;
+      width: 500px;
+      padding-top: 70px;
+      padding-bottom: 70px;
+    }
+  </style>
+
 </head>
 <body>
 
@@ -31,27 +73,35 @@ session_start();
   ?>
 
   <div class="content">
-    <form action="">
-      <div>
-        <label>Name</label>
-        <input type="text" name="name">
-      </div>
-      <div>
-        <label>Email</label>
-        <input type="email" name="email">
-      </div>
-      <div>
-        <label>Phone</label>
-        <input type="number" name="phone">
-      </div>
-      <div>
-        <label>Password</label>
-        <input type="password" name="password">
-      </div>
-      <div>
-        <input type="submit" name="update_profile">
-      </div>
-    </form>
+    <center>
+
+      <h1>Update Profile</h1>
+      <br><br>
+
+      <form action="#" method="POST">
+        <div class="div_deg">
+          <div>
+            <!-- <label>Name</label>
+            <input type="text" name="name" value="<?php echo "{$info['username']}" ?>">
+          </div> -->
+          <div>
+            <label>Email</label>
+            <input type="email" name="email" value="<?php echo "{$info['email']}" ?>">
+          </div>
+          <div>
+            <label>Phone</label>
+            <input type="number" name="phone" value="<?php echo "{$info['phone']}" ?>">
+          </div>
+          <div>
+            <label>Password</label>
+            <input type="text" name="password" value="<?php echo "{$info['password']}" ?>">
+          </div>
+          <div>
+            <input type="submit" class="btn btn-info" name="update_profile" value="Update">
+          </div>
+        </div>
+      </form>
+    </center>
   </div>
   
 </body>
