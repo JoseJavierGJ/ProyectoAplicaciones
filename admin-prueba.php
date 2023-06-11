0d43f1a4293b4fd7c1a78e0235bbc8d5dadd90fa
JoseJavierGJ <jjosejavier@hotmail.com>
2023-06-11 14:19:27 -0600

    Asignar alumnos a cursos

---
+++ admin-prueba.php
@@ -0,0 +1,127 @@
+<?php
+
+session_start();
+error_reporting(0);
+
+if (!isset($_SESSION['username'])) {
+  header("location:login.php");
+} elseif ($_SESSION['usertype'] == 'student') {
+  header("location:login.php");
+}
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "schoolproject";
+
+$data = mysqli_connect($host, $user, $password, $db);
+$sql = "SELECT * FROM course";
+$result = mysqli_query($data, $sql);
+
+if (isset($_GET['course_id'])) {
+  $c_id = $_GET['course_id'];
+  $sql2 = "DELETE FROM course WHERE id='$c_id' ";
+  $result2 = mysqli_query($data, $sql2);
+
+  if ($result2) {
+    header('location:admin_view_course.php');
+  }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Dashboard</title>
+  <link rel="icon" href="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3a9f7539-9d01-4e36-b27d-254409ac16c9/d9e64l5-e25b4b91-9738-470b-bf7f-b75878a85d34.png/v1/fill/w_16,h_16/16x16_free_pixel_cookie_by_mintiestea_d9e64l5-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTYiLCJwYXRoIjoiXC9mXC8zYTlmNzUzOS05ZDAxLTRlMzYtYjI3ZC0yNTQ0MDlhYzE2YzlcL2Q5ZTY0bDUtZTI1YjRiOTEtOTczOC00NzBiLWJmN2YtYjc1ODc4YTg1ZDM0LnBuZyIsIndpZHRoIjoiPD0xNiJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.i7QshEPrKPq9V52UMdtCNgPc491I6lf6elJY4k-0_NI" type="image/png">
+
+  <?php
+  include 'admin_css.php';
+  ?>
+
+  <style type="text/css">
+    .table_th {
+      padding: 20px;
+      font-size: 20px;
+    }
+
+    .table_td {
+      padding: 20px;
+      background-color: #CDF7F6;
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <?php
+  include 'admin_sidebar.php';
+  ?>
+
+  <div class="content">
+    <center><br>
+      <h1>Ver información de los cursos</h1>
+      <table border="1px">
+        <tr>
+          <th class="table_th" style="text-align: center;">Materia</th>
+          <th class="table_th" style="text-align: center;">Descripción</th>
+          <th class="table_th" style="text-align: center;">Imagen</th>
+          <th class="table_th" style="text-align: center;">Borrar</th>
+          <th class="table_th" style="text-align: center;">Actualizar</th>
+          <th class="table_th" style="text-align: center;">Asignar</th>
+        </tr>
+        <?php
+        while ($info = $result->fetch_assoc()) {
+
+        ?>
+
+          <tr>
+            <td class="table_td" style="text-align: center;">
+              <?php echo "{$info['name']}" ?>
+            </td>
+            <td class="table_td" style="text-align: justify; width:350px;">
+              <?php echo "{$info['description']}" ?>
+            </td>
+            <td class="table_td">
+              <img height="100px" width="120px" src=" <?php echo "{$info['image']}" ?>">
+            </td>
+            <td class="table_td">
+              <?php
+              echo "
+              <a onClick=\"javascript:return confirm('Are you sure you want to delete this course?');\" class='btn btn-danger' href='admin_view_course.php?course_id={$info['id']}'>Delete❌</a>"
+              ?>
+            </td>
+            <td class="table_td">
+              <?php
+              echo "
+              <a href='admin_update_course.php?course_id={$info['id']}' class='btn btn-info'>Update👨🏽‍🤝‍👨🏻</a>";
+              ?>
+            </td>
+            <td class="table_td">
+              <?php
+              echo "
+              <a href='assign_student.php?course_id={$info['id']}' class='btn btn-primary'>Asignar estudiante👥</a>";
+              ?>
+            </td>
+          </tr>
+
+        <?php
+
+        }
+
+        ?>
+
+      </table>
+    </center>
+  </div>
+
+</body>
+
+</html>
---
+++ assign_student.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+error_reporting(0);
+
+if (!isset($_SESSION['username'])) {
+    header("location: login.php");
+}
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "schoolproject";
+
+$data = mysqli_connect($host, $user, $password, $db);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user_id = $_POST['user_id'];
+    $course_id = $_POST['course_id'];
+
+    $sql = "INSERT INTO course_student (user_id, course_id) VALUES ('$user_id', '$course_id')";
+    $result = mysqli_query($data, $sql);
+
+    if ($result) {
+        header("location: assign_student.php");
+        exit();
+    } else {
+        echo "Error: " . mysqli_error($data);
+    }
+}
+
+$sql = "SELECT * FROM course";
+$result = mysqli_query($data, $sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assign Student to Course</title>
+    <link rel="icon" href="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3a9f7539-9d01-4e36-b27d-254409ac16c9/d9e64l5-e25b4b91-9738-470b-bf7f-b75878a85d34.png/v1/fill/w_16,h_16/16x16_free_pixel_cookie_by_mintiestea_d9e64l5-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTYiLCJwYXRoIjoiXC9mXC8zYTlmNzUzOS05ZDAxLTRlMzYtYjI3ZC0yNTQ0MDlhYzE2YzlcL2Q5ZTY0bDUtZTI1YjRiOTEtOTczOC00NzBiLWJmN2YtYjc1ODc4YTg1ZDM0LnBuZyIsIndpZHRoIjoiPD0xNiJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.i7QshEPrKPq9V52UMdtCNgPc491I6lf6elJY4k-0_NI">
+    <?php include 'admin_css.php'; ?>
+    <style type="text/css">
+        label {
+            display: inline-block;
+            width: 150px;
+            text-align: right;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+
+        .form_deg {
+            background-color: #CDF7F6;
+            width: 600px;
+            padding-top: 70px;
+            padding-bottom: 70px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include 'admin_sidebar.php'; ?>
+
+    <div class="content">
+        <center>
+            <h1>Assign Student to Course</h1>
+            <form class="form_deg" method="POST" action="assign_student.php">
+                <div>
+                    <label for="user_id">Student ID:</label>
+                    <input type="text" name="user_id" id="user_id" required>
+                </div>
+                <div>
+                    <label for="course_id">Course ID:</label>
+                    <select name="course_id" id="course_id" required>
+                        <option value="">Select Course</option>
+                        <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+                            <option value="<?php echo $row['id']; ?>"><?php echo $row['name']; ?></option>
+                        <?php endwhile; ?>
+                    </select>
+                </div>
+                <div>
+                    <input type="submit" class="btn btn-info" value="Assign Student">
+                </div>
+            </form>
+        </center>
+    </div>
+</body>
+
+</html>
